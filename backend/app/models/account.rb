ca14bad15716f5c4f88486779e0dcef5a7275c17
sedx876 <swatkins876@gmail.com>
2020-09-11 14:02:47 -0400

    updated Account model

--- backend/app/models/account.rb
+++ backend/app/models/account.rb
@@ -1,4 +1,34 @@
 class Account < ApplicationRecord
   has_many :transactions
   validates :name, :balance, presence: true
+
+  def update_balance(transaction)
+    if transaction.kind == 'deposit'
+      self.balance = self.balance + transaction.amount
+      self.save
+    elsif transaction.kind == 'withdraw'
+      if self.balance >= transaction.amount
+        self.balance = self.balance - transaction.amount
+        self.save
+      else
+        return 'Balance too low.'
+      end
+    end
+  end
+
+  def update_balance_on_delete(transaction)
+    if transaction.kind == 'deposit'
+      if self.balance >= transaction.amount
+        self.balance = self.balance - transaction.amount
+        self.save
+      else
+        return 'Balance too low.'
+      end
+    elsif transaction.kind == 'withdraw'
+        self.balance = self.balance + transaction.amount
+        self.save
+    end
+  end
+
+
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -2,3 +2,8 @@
 # Examples:
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+account = Account.create(name: 'Checking', balance: 1000)
+
+transaction = Transaction.create(account_id: 1, amount: 10, kind: 'deposit', date: DateTime.now, description: 'Found $10')
+
+transaction_two = Transaction.create(account_id: 1, amount: 10, kind: 'withdraw', date: DateTime.now, description: 'Found $10')
