c787468495cb47ddb147a5098189a573eeba852d
sedx876 <swatkins876@gmail.com>
2020-09-11 13:21:52 -0400

    Api::V1 namespacing

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :transactions
-  resources :accounts
+  namespace :api do
+    namespace :v1 do
+      resources :accounts do
+          resources :transactions
+      end
+    end
+  end
+
 end
