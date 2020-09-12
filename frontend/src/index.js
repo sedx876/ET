0dc67e405cc751883587308fa36cd1edc0e73a3e
sedx876 <swatkins876@gmail.com>
2020-09-12 11:51:34 -0400

    Added accountReducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import accountReducer from './reducers/accountReducer'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = createStore(accountReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
