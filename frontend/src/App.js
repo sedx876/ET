1304ef1eca32a66862a06c7c1ad0a4cd3ddc67b4
sedx876 <swatkins876@gmail.com>
2020-09-12 10:48:16 -0400

    Accounts and Transactions serializers added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,12 @@ import React from 'react';
 
 class App extends React.Component {
 
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/accounts')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
   render() {
     return (
       <div className="App">
