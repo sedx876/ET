93e07fb8d77cb69bce2ba176572f6160fcac8862
sedx876 <swatkins876@gmail.com>
2020-09-12 14:12:39 -0400

    mapStateToProps in AccountsContainer

--- frontend/src/actions/fetchAccounts.js
+++ frontend/src/actions/fetchAccounts.js
@@ -1,6 +1,6 @@
 export function fetchAccounts() {
   return (dispatch) => {
-    fetch('http://localhost:3000/api/v1/accounts')
+    fetch('http://localhost:3001/api/v1/accounts')
     .then(resp => resp.json())
     .then(accounts => dispatch({
       type: 'FETCH_ACCOUNTS',
--- frontend/src/containers/AccountsContainer.js
+++ frontend/src/containers/AccountsContainer.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 import Accounts from '../components/Accounts'
 import AccountInput from '../components/AccountInput'
+import {connect} from 'react-redux'
+import {fetchAccounts} from '../actions/fetchAccounts'
+
 
 
 class AccountsContainer extends Component {
+
+  componentDidMount() {
+    this.props.fetchAccounts()
+  }
+
   render() {
     return (
       <div>
@@ -14,4 +22,10 @@ class AccountsContainer extends Component {
   }
 }
 
-export default AccountsContainer
+const mapStateToProps = state => {
+  return {
+    accounts: state.accounts
+  }
+}
+
+export default connect(mapStateToProps, {fetchAccounts})(AccountsContainer)
