8584c290bfc1388b2e9c0d28041bd571eedae236
sedx876 <swatkins876@gmail.com>
2020-09-12 10:58:45 -0400

    Added react-redux redux redux-thunk react-router-dom dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,6 @@ import React from 'react';
 
 class App extends React.Component {
 
-  componentDidMount() {
-    fetch('http://localhost:3001/api/v1/accounts')
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }
 
   render() {
     return (
