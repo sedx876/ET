5354c0cf7af149d0f9df6e33907303c5e696bb8a
sedx876 <swatkins876@gmail.com>
2020-09-14 10:34:49 -0400

    Account.js component added

--- frontend/src/actions/addAccount.js
+++ frontend/src/actions/addAccount.js
@@ -1,7 +1,7 @@
 export const addAccount = (data) => {
 
   return (dispatch) => {
-    fetch('http://localhost:3000/api/v1/accounts', {
+    fetch('http://localhost:3001/api/v1/accounts', {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
